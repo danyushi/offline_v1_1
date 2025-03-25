04859eae257a20cec51461ac05ccdf42e0bbccdb
offline <git@github.com:danyushi/offline_v1.git>
2025-03-25 10:16:35 +0800

    250324

---
+++ shell/quanbu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t activity_info
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t activity_order
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t activity_rule
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t activity_sku
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t base_category1
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t base_category2
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t base_category3
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t base_dic
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t base_province
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t base_region
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t base_trademark
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t cart_info
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t comment_info
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t coupon_info
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t coupon_use
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t date_info
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t favor_info
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t holiday_info
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t holiday_year
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t order_detail
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t order_info
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t order_refund_info
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t order_status_log
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t payment_info
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t sku_info
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t spu_info
+python3 /opt/soft/datax/job/2207A/danyu_shi/gen_import_config.py -d dev_realtime_v1_danyu_shi -t user_info
+
+
---
+++ sql/sq.sql
@@ -0,0 +1,9 @@
+use dev_realtime_v1_danyu_shi;
+
+select * from ods_order_info;
+select * from ods_order_detail;
+select * from ods_sku_info ;
+select * from ods_spu_info;
+select * from ods_payment_info;
+
+select * from ods_user_info;
