c0bf0f81669388f2225029f4b79d8705da1cfa44
offline <git@github.com:danyushi/offline_v1.git>
2025-03-25 20:21:05 +0800

    2503245

--- sql/sq.sql
+++ sql/sq.sql
@@ -5,5 +5,18 @@ select * from ods_order_detail;
 select * from ods_sku_info ;
 select * from ods_spu_info;
 select * from ods_payment_info;
-
 select * from ods_user_info;
+
+
+select * from dwd_order_info;
+select * from dwd_order_detail;
+select * from dwd_sku_info;
+select * from dwd_spu_info;
+
+
+select * from dws_with;
+select * from dws_order_count;
+
+
+select * from ads_gmv_amount;
+select * from ads_sku_lv;
