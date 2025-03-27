30842cae291f693498ec47b20e9d19a67ca2652f
offline <git@github.com:danyushi/offline_v1.git>
2025-03-27 20:50:44 +0800

    25-03-27

---
+++ sql/tms01.sql
@@ -0,0 +1,7 @@
+select * from ods_order_detail_tms01;
+select * from ods_order_info_tms01;
+select * from ods_order_refund_info_tms01;
+select * from ods_base_province_tms01;
+select * from ods_base_region_tms01;
+select * from ods_payment_info_tms01;
+select * from ods_refund_payment_tms01;
